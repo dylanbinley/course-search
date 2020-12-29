de281a44f7d25e7aa775d11882561a41b385043a
Dylan Binley <dylanbinley@gmail.com>
2020-12-28 23:39:47 -0500

    Refactoring

--- html-css-javascript/search-courses.js
+++ html-css-javascript/search-courses.js
@@ -1,112 +1,60 @@
-var rmpdata = [];
-var mydata= [];
-var professorlink = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid='
+var rmpData, courses;
+
 function onStart(data){
-    mydata = JSON.parse(data);
-    for (var pageNum = 1; pageNum < 132; pageNum++){ 
+    courses = JSON.parse(data);
+    rmpData = [];
+    const TOTAL_PAGES = 132;
+    for (var pageNum = 1; pageNum < TOTAL_PAGES; pageNum++){
         var text = 'https://www.ratemyprofessors.com/find/professor/?department=&institution=The+University+of+North+Carolina+at+Chapel+Hill&page=' + pageNum + '&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid=1232&sortBy='
         $.getJSON(text, function(data){
             var professors = data.professors;
             for (var i = 0; i< professors.length; i++){
-                rmpdata.push(professors[i]);
+                rmpData.push(professors[i]);
             }
             loadLocalClasses()
         });
     }
 }
+
 function loadLocalClasses(){
-    var input = document.getElementById("search").value;
-    var parsedInput = input.split(/(?:-| )+/);
-    if (parsedInput.length == 1){
-        parsedInput[1] = 0;
-        parsedInput[2] = 999;
-    }
-    else if (parsedInput.length == 3 && parsedInput[2] > 999)
-    {
-        parsedInput[2] = 999;
-    }
-    else if (parsedInput.length == 2){
-        parsedInput[2] = parsedInput[1];
-    }
-    let tble = document.querySelector("table");
-    tble.innerHTML = ` <thead>
-                <tr>
-                    <th>Course Number</th>
-                    <th>Course Name</th>
-                    <th>Time</th>
-                    <th>Professor</th>
-                    <th>Rating</th>
-                    <th>Status</th>
-                </tr>
-            </thead>`;
-    for (var i = 0; i< mydata.length; i++){
-        if (mydata[i].time != "TBA" && mydata[i].section < 100 && mydata[i].subject.toLowerCase() == parsedInput[0].toLowerCase() && mydata[i].number >= parsedInput[1] && mydata[i].number <= parsedInput[2]){
-            let row = tble.insertRow();
-            let cell = row.insertCell();
-            let txt = document.createTextNode(mydata[i].subject + " " + mydata[i].number + " " + mydata[i].section);
-            cell.appendChild(txt);
+    var parsedInput = splitInput(document.getElementById("search").value); 
+    let table= document.querySelector("table");
+    table.innerHTML = ` <thead>
+                            <tr>
+                                <th>Course Number</th>
+                                <th>Course Name</th>
+                                <th>Time</th>
+                                <th>Professor</th>
+                                <th>Rating</th>
+                                <th>Status</th>
+                            </tr>
+                       </thead>
+                     `;
+    for (var i = 0; i < courses.length; i++){
+        if (courses[i].time != "TBA" 
+            && courses[i].section < 100 //exclude recitations 
+            && courses[i].subject.toLowerCase() == parsedInput[0].toLowerCase() 
+            && courses[i].number >= parsedInput[1] 
+            && courses[i].number <= parsedInput[2]){
 
-            cell = row.insertCell();
-            txt = document.createTextNode(mydata[i].name);
-            cell.appendChild(txt);
-
-            cell = row.insertCell();
-            txt = document.createTextNode(mydata[i].days + ' ' + mydata[i].time);
-            cell.appendChild(txt);
-
-            cell = row.insertCell();
-            let a = document.createElement('a');
-            txt = document.createTextNode(mydata[i].tFname + " " + mydata[i].tLname);
-            a.appendChild(txt);
-            a.title = txt;
-
-            let hasRating = false;
-            for (var j = 0; j< rmpdata.length; j++){
-                if (mydata[i].tLname.toLowerCase() == rmpdata[j].tLname.toLowerCase() && mydata[i].tFname == rmpdata[j].tFname){
-                    a.href = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=' + rmpdata[j].tid
-                    cell.appendChild(a);
-                    cell= row.insertCell();
-                    cell.appendChild(createRating(rmpdata[j].overall_rating));
-                    hasRating = true;
-                }
-
-            }
-            if (!hasRating){
-                cell.appendChild(txt);
-                let cell3 = row.insertCell();
-                cell3.appendChild(createRating("N/A"));
-            }
-
-            cell = row.insertCell();
-            var enrollment = mydata[i].enrollment;
-            var capacity = mydata[i].capacity;
-            var class_status;
-            if (enrollment == "N/A" || capacity == "N/A"){
-                class_status = "unknown";
-            }
-            else if (enrollment < capacity){
-                class_status = "open"
-            }
-            else {
-                var waitlist_total = mydata[i].waitlist_total;
-                var waitlist_capacity = mydata[i].waitlist_capacity;
-                if (waitlist_total == "N/A" || waitlist_capacity == "N/A"){
-                    class_status = "closed";
-                }
-                else if (waitlist_capacity > waitlist_total){
-                    class_status= "waitlist";
-                }
-                else{
-                    class_status = "closed";
-                }
-            }
-            cell.appendChild(createImage(class_status));
+            let courseNumber= document.createTextNode(courses[i].subject + " " + courses[i].number + " " + courses[i].section);
+            let courseName = document.createTextNode(courses[i].name);
+            let time = document.createTextNode(courses[i].days + ' ' + courses[i].time);
+            let professor = getProfessorObj(courses[i])
+            let classStatus= getClassStatus(courses[i])
+            let row = table.insertRow();
+            row.insertCell().appendChild(courseNumber);
+            row.insertCell().appendChild(courseName);
+            row.insertCell().appendChild(time);
+            row.insertCell().appendChild(professor.name);
+            row.insertCell().appendChild(professor.rating);
+            row.insertCell().appendChild(classStatus);
         }
     }
 }
+
 function createImage(type){
-    var x = document.createElement("img");
-    var src; 
+    var img, src; 
     switch(type) {
         case "open":
             src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Ski_trail_rating_symbol-green_circle.svg/1024px-Ski_trail_rating_symbol-green_circle.svg.png"
@@ -117,47 +65,110 @@ function createImage(type){
         default:
             src = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Ski_trail_rating_symbol-blue_square.svg/900px-Ski_trail_rating_symbol-blue_square.svg.png"
     } 
-    x.setAttribute("src", src); 
-    x.setAttribute("width", "20");
-    x.setAttribute("height", "20");
-    x.setAttribute("title", type); 
-    x.setAttribute("alt", type); 
-    return x
+    img = document.createElement("img");
+    img.setAttribute("src", src); 
+    img.setAttribute("width", "20");
+    img.setAttribute("height", "20");
+    img.setAttribute("title", type); 
+    img.setAttribute("alt", type); 
+    return img 
 }
+
 function createRating(rating){
-    if (rating == "N/A"){
-        color = "gray"
+    var div, color;
+    switch(Math.ceil(rating-.5)){
+        case 5: //5-4.5
+            color = "green"
+            break;
+        case 4: //4.49-3.5
+            color = "rgb(0,230,0)";
+            break;
+        case 3: //3.49-2.5
+            color = "orange"
+            break;
+        case 2: //2.49-1.5
+            color = "rgb(255,0, 0)";
+            break;
+        case 1: //<1.5
+        case 0:
+            color = "rgb(200,0, 0)";
+            break;
+        default: //"N/A"
+            color = "gray"
+            break;
     }
-    else if (rating >= 4.5){
-        color = "green"
-    }
-    else if(rating >= 3.5){
-        color = "rgb(0,230,0)";
-    }
-    else if (rating >= 2.5){
-        color = "orange"
-
-    }
-    else if (rating >= 1.5){
-        color = "rgb(255,0, 0)";
-    }
-
-    else {
-        color = "rgb(200,0, 0)";
-    }
-    txt= document.createElement("p");
-    txt.innerHTML = rating 
-    txt.style.color ="white"
-    txt.style.backgroundColor = color
-    txt.style.width="35px"
-    txt.style.paddingTop = "5px"
-    txt.style.paddingBottom = "5px"
-    txt.style.marginRight = "auto"
-    txt.style.marginLeft= "auto"
-    txt.style.fontWeight = "bold"
-    return txt;
+    div = document.createElement("div");
+    div.innerHTML = rating 
+    div.style.color ="white"
+    div.style.backgroundColor = color
+    div.style.width="35px"
+    div.style.paddingTop = "5px"
+    div.style.paddingBottom = "5px"
+    div.style.marginRight = "auto"
+    div.style.marginLeft= "auto"
+    div.style.fontWeight = "bold"
+    return div;
 }
-function logText(){
-    var input = document.getElementById("search").value;
-    console.log(input);
+
+function splitInput(input){
+    var split = input.split(/(?:-| )+/) //split on space/dash
+    split = split.filter(function(value, index, arr){ 
+        return value !== ""; //remove duplicate spaces/dashes
+    });
+    if (split.length == 1){ //search entire department (ex. COMP) 
+        split[1] = 0;
+        split[2] = 999;
+    }
+    else if (split.length == 2){ //search specific class (ex. COMP 211)
+        split[2] = split[1];
+    }
+    return split;
+}
+
+function getProfessorObj(course){
+    let profName = document.createTextNode(course.tFname + " " + course.tLname);
+    let info = getRateMyProfessorsInfo(course); 
+    if (info != null){
+        let link = document.createElement('a');
+        link.href = 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=' + info.tid
+        link.appendChild(profName);
+        return { name : link, rating: createRating(info.rating) }
+    }
+    return { name: profName, rating: createRating("N/A") }
+}
+
+function getRateMyProfessorsInfo(course){
+    for (var j = 0; j< rmpData.length; j++){
+        if (course.tLname.toLowerCase() == rmpData[j].tLname.toLowerCase() 
+            && course.tFname.toLowerCase() == rmpData[j].tFname.toLowerCase()){
+            return {id: rmpData[j].tid, rating: rmpData[j].overall_rating}
+        }
+    }
+    return null;
+}
+
+function getClassStatus(course){
+    var class_status;
+    var enrollment = course.enrollment;
+    var capacity = course.capacity;
+    if (enrollment == "N/A" || capacity == "N/A"){
+        class_status = "unknown";
+    }
+    else if (enrollment < capacity){
+        class_status = "open"
+    }
+    else {
+        var waitlist_total = course.waitlist_total;
+        var waitlist_capacity = course.waitlist_capacity;
+        if (waitlist_total == "N/A" || waitlist_capacity == "N/A"){
+            class_status = "closed";
+        }
+        else if (waitlist_capacity > waitlist_total){
+            class_status= "waitlist";
+        }
+        else{
+            class_status = "closed";
+        }
+    }
+    return createImage(class_status);
 }
